44ab5c86d5b331226fc90f2efd6d438421009f4b
yerevin <sirmmate@gmail.com>
2017-01-21 00:59:46 +0100

    modal

--- src/Stuff.js
+++ src/Stuff.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import ReactModal from 'react-modal';
 import './Stuff.css';
 
 class Stuff extends Component {
 	constructor() {
 	    super();
 	    this.state = {
+      		showModal: false,
 	    	addStatus: false,
 	    	addLabel:false,
 	    	addedLabel: false,
@@ -15,7 +17,7 @@ class Stuff extends Component {
 	    	labelName: '',
 	    	stuffName: '',
 	    	stuffPrice: '',
-	    	stuffAmount: '',
+	    	stuffAmount: ''
 	    };
 	}
 
@@ -62,7 +64,7 @@ class Stuff extends Component {
 							return (
 								<div className="col-sm-4 pull-left">
 									<h3>{this.state.stuff[itemIndex].labelName}</h3>
-									<button className="btn" type="button" onClick={addStatus => this.setState({ addLabel: true, selectedLabel: itemIndex })}>Add</button>
+									<button className="btn" type="button"onClick={addStatus => this.setState({ addLabel: true, showModal: true, selectedLabel: itemIndex })}>Add</button>
 									{(this.state.addedLabel) ?
 										this.state.stuff[itemIndex].stuffArray.map(function(stuffItem, stuffIndex) {
 						                    return (
@@ -80,27 +82,66 @@ class Stuff extends Component {
 					}
 					</div>
 				</div>
-				<div className="container pull-right add-stuff">
-				{ (this.state.addLabel) ? 
-					<div className="form-container">
-						<form>
-							<div className="form-group">
-								<label>Nazwa:</label>
-								<input type="text" className="form-control PlaceText" onChange={stuffName => this.setState({ stuffName:stuffName.target.value })} value={this.state.stuffName}/>
+			<ReactModal 
+	           isOpen={this.state.showModal}
+	           contentLabel="Inline Styles Modal Example"
+	           className="Modal"
+	           overlayClassName="Overlay"
+	        >
+				      <div className="modal-header">
+				        <h4 className="modal-title" id="myModalLabel">Dodaj przedmiot</h4>
+				        <span className="close pull-right" onClick={()=> this.setState({showModal: false})} aria-hidden="true">&times;</span>
+				      </div>
+				<div className="modal-form-container">
+							<form>
+								<div className="form-group">
+									<label>Nazwa:</label>
+									<input type="text" className="form-control PlaceText" onChange={stuffName => this.setState({ stuffName:stuffName.target.value })} value={this.state.stuffName}/>
+								</div>
+								<div className="form-group">
+									<label>Cena:</label>
+									<input type="text" className="form-control PlaceText" onChange={stuffPrice => this.setState({ stuffPrice:stuffPrice.target.value })} value={this.state.stuffPrice}/>
+								</div>
+								<div className="form-group">
+									<label>Ilość:</label>
+									<input type="text" className="form-control PlaceText" onChange={stuffAmount => this.setState({ stuffAmount:stuffAmount.target.value })} value={this.state.stuffAmount}/>
+								</div>
+							</form> 
 							</div>
-							<div className="form-group">
-								<label>Cena:</label>
-								<input type="text" className="form-control PlaceText" onChange={stuffPrice => this.setState({ stuffPrice:stuffPrice.target.value })} value={this.state.stuffPrice}/>
+				      <div className="modal-footer">
+				      	<button className="btn btn-success" type="button" onClick={this.addStuff.bind(this)}>Add</button>
+				      </div>
+	        </ReactModal>			
+				<div className="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+				  <div className="modal-dialog" role="document">
+				    <div className="modal-content">
+				      <div className="modal-header">
+				        <h4 className="modal-title" id="myModalLabel">Dodaj przedmiot</h4>
+				        <span className="close pull-right" data-dismiss="modal" aria-hidden="true">&times;</span>
+				      </div>
+				      <div className="modal-body">
+							<div className="modal-form-container">
+							<form>
+								<div className="form-group">
+									<label>Nazwa:</label>
+									<input type="text" className="form-control PlaceText" onChange={stuffName => this.setState({ stuffName:stuffName.target.value })} value={this.state.stuffName}/>
+								</div>
+								<div className="form-group">
+									<label>Cena:</label>
+									<input type="text" className="form-control PlaceText" onChange={stuffPrice => this.setState({ stuffPrice:stuffPrice.target.value })} value={this.state.stuffPrice}/>
+								</div>
+								<div className="form-group">
+									<label>Ilość:</label>
+									<input type="text" className="form-control PlaceText" onChange={stuffAmount => this.setState({ stuffAmount:stuffAmount.target.value })} value={this.state.stuffAmount}/>
+								</div>
+							</form> 
 							</div>
-							<div className="form-group">
-								<label>Ilość:</label>
-								<input type="text" className="form-control PlaceText" onChange={stuffAmount => this.setState({ stuffAmount:stuffAmount.target.value })} value={this.state.stuffAmount}/>
-							</div>
-							<button className="btn" type="button" onClick={this.addStuff.bind(this)}>Add</button>
-						</form> 
-					</div>
-					 : null
-				}
+				      </div>
+				      <div className="modal-footer">
+				      	<button className="btn btn-success" type="button" onClick={this.addStuff.bind(this)}>Add</button>
+				      </div>
+				    </div>
+				  </div>
 				</div>
 				<button className="btn pull-right PlaceSave" onClick={this.saveUp.bind(this)}>Save Up</button>
       		</div>
@@ -109,3 +150,20 @@ class Stuff extends Component {
 }
 
 export default Stuff;
+
+							/*<div className="modal-form-container">
+							<form>
+								<div className="form-group">
+									<label>Nazwa:</label>
+									<input type="text" className="form-control PlaceText" onChange={stuffName => this.setState({ stuffName:stuffName.target.value })} value={this.state.stuffName}/>
+								</div>
+								<div className="form-group">
+									<label>Cena:</label>
+									<input type="text" className="form-control PlaceText" onChange={stuffPrice => this.setState({ stuffPrice:stuffPrice.target.value })} value={this.state.stuffPrice}/>
+								</div>
+								<div className="form-group">
+									<label>Ilość:</label>
+									<input type="text" className="form-control PlaceText" onChange={stuffAmount => this.setState({ stuffAmount:stuffAmount.target.value })} value={this.state.stuffAmount}/>
+								</div>
+							</form> 
+							</div>*/
