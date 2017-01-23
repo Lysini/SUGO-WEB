b99478a7fdd9d0cb4b998b4bc67550ee897ccac7
Lysini <kubammmm@interia.pl>
2017-01-23 22:39:21 +0100

    xD

--- src/People.js
+++ src/People.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import ReactModal from 'react-modal';
 
 
 class People extends Component {
 	constructor() {
 		super();
 		this.state = {
+			showModal: false,
 			added: false,
-			inputSex: 'men',
+			peopleSex: 'men',
 			numberOfUsers: 0,
 			peopleWomen: [],
 			peopleMen: []
@@ -24,11 +26,11 @@ class People extends Component {
 	saveUpUser() {
 		let person = {
 			peopleName: this.state.inputName,
-			peopleSex: this.state.inputSex,
+			peopleSex: this.state.peopleSex,
 			peopleNote: this.state.inputNote,
 			numberOfUsers: this.state.numberOfUsers+1
 		};
-		if(this.state.inputSex === "men") {
+		if(this.state.peopleSex === "men") {
 			this.state.peopleMen.push(person);
 		}
 		else {
@@ -36,91 +38,98 @@ class People extends Component {
 		}
 		this.setState({ 
 			added: true, 
+			showModal: false,
 			numberOfUsers: this.state.numberOfUsers+1,
 			inputName: '',
 			inputNote: '' 
 		});
 	}
 
+	multipleAdd(){
+		if(this.state.multipleAddStatus===false){
+			this.setState({multipleAddStatus: true})
+		}
+		else if(this.state.multipleAddStatus===true){
+			this.setState({multipleAddStatus: false})
+		}
+	}
+
   render() {
     return (
 		<div className="openedPlace">
 			<div className="container pull-left">
-				<h1>Info o Organizatorze</h1>
-				<p>Imie</p>
-				<p>Nazwisko</p>
-				<p>Info</p>
+				<div className="col-sm-4 people-info">
+					<h1>Info o Organizatorze</h1>
+					<p>Imie</p>
+					<p>Nazwisko</p>
+					<p>Info</p>
+				</div>
 			</div>
 			<h className="title">People</h>
 	      	<div className="PlaceClose" onClick={this.props.onClose}>
 	     		<div className="close-left"></div>
 	      		<div className="close-right"></div>
-	      	</div>
-	      	<div className="container pull-left">
-	      		<div className="form-container">
-			      <form>
-				    <div className="form-group">
-						<label>Imię i nazwisko lub pseudonim:</label>
-						<input type="text" className="form-control PlaceText" onChange={inputName => this.setState({ inputName:inputName.target.value })} value={this.state.inputName}/>
+	      	</div> 
+			<div className="container ">
+				<div className="row">
+					<div className="col-sm-12 col-md-6 text-center">
+					   	<h1 className="people-men">Mężczyźni <button className="btn " onClick={showModal => this.setState({showModal: true, peopleSex: 'men'})}>+</button></h1>
+					   	<div className="people-men-1">
+							{(this.state.added) ? 
+				                this.state.peopleMen.map((item, itemIndex) => {
+									return (
+										<div>
+											<p>Imie: {this.state.peopleMen[itemIndex].peopleName}</p> 
+								          	<p>Notka: {this.state.peopleMen[itemIndex].peopleNote}</p>
+								       	</div>
+							        );
+							    }) : null
+						    }
+						</div>
 					</div>
-			          <select onChange={inputSex => this.setState({ inputSex:inputSex.target.value })} value={this.state.inputSex}>
-			            <option value="men">Mężczyzna</option>
-			            <option value="women">Kobieta</option>
-			          </select>
-				    <div className="form-group">
-						<label>Notatki:</label>
-						<input type="text" className="form-control PlaceText" onChange={inputNote => this.setState({ inputNote:inputNote.target.value })} value={this.state.inputNote} />
+					<div className="col-sm-12 col-md-6 text-center">
+						<h1 className="people-women">Kobiety <button className="btn " onClick={showModal => this.setState({showModal: true, peopleSex: 'women'})}>+</button></h1>   
+						<div className="people-women-1">
+							{(this.state.added) ? 
+			               		this.state.peopleWomen.map((item, itemIndex) => {
+									return (
+										<div>
+							            	<p>Imie: {this.state.peopleWomen[itemIndex].peopleName}</p> 
+							           		<p>Notka: {this.state.peopleWomen[itemIndex].peopleNote}</p> 
+							        	</div>
+						        	);
+						    	}) : null
+					    	}
+					    </div>
 					</div>
-					<button className="btn" type="button" onClick={this.saveUpUser.bind(this)}>Save</button>
-				   </form>
-			</div>
-		<table className="table"> 
-		    <caption>Mężczyźni</caption> 
-			    <thead> 
-			        <tr> 
-			            <th></th> 
-			            <th>Dane uczestnika</th>  
-			        </tr> 
-			    </thead> 
-		    <tbody> 
-			{ (this.state.added) ? 
-                this.state.peopleMen.map((item, itemIndex) => {
-					return (
-						<tr key={itemIndex}> 
-				            <td>{this.state.peopleMen[itemIndex].peopleName}</td> 
-				            <td>{this.state.peopleMen[itemIndex].peopleSex}</td>
-				            <td>{this.state.peopleMen[itemIndex].peopleNote}</td> 
-						</tr>
-			        );
-			    }) : null
-		    }
-		    </tbody> 
-		</table>
-		<table className="table"> 
-		    <caption>Kobiety</caption> 
-			    <thead> 
-			        <tr> 
-			            <th></th> 
-			            <th>Dane uczestnika</th>  
-			        </tr> 
-			    </thead> 
-		    <tbody> 
-			{ (this.state.added) ? 
-                this.state.peopleWomen.map((item, itemIndex) => {
-					return (
-						<tr key={itemIndex}> 
-				            <td>{this.state.peopleWomen[itemIndex].peopleName}</td> 
-				            <td>{this.state.peopleWomen[itemIndex].peopleSex}</td>
-				            <td>{this.state.peopleWomen[itemIndex].peopleNote}</td> 
-						</tr>
-			        );
-			    }) : null
-		    }
-		    </tbody> 
-		</table>  
-				<button className="btn PlaceSave" onClick={this.saveUp.bind(this)}>Next</button>
-	      	</div>
-      </div>
+				</div>
+			</div>	
+			<ReactModal 
+		        isOpen={this.state.showModal}
+		        contentLabel="Inline Styles Modal Example"
+		        className="Modal"
+		        overlayClassName="Overlay">
+					<div className="modal-header text-center">
+					    <h4 className="modal-title" id="myModalLabel">Dodaj Uczestnika</h4>
+					    <span className="pull-right" onClick={()=> this.setState({showModal: false})} aria-hidden="true">X</span>
+					</div>
+					<div className="modal-form-container">
+						<form>
+				   			<div className="form-group text-center">
+								<label>Imię i nazwisko lub pseudonim:</label>
+								<input type="text" className="form-control PlaceText" onChange={inputName => this.setState({ inputName:inputName.target.value })} value={this.state.inputName}/>
+							</div>
+							<div className="form-group text-center">
+								<label>Notatki:</label>
+								<input type="text" className="form-control PlaceText" onChange={inputNote => this.setState({ inputNote:inputNote.target.value })} value={this.state.inputNote} />
+							</div>
+							<button className="btn pull-right" type="button" onClick={this.saveUpUser.bind(this)}>Save</button>
+						</form>
+					</div>
+		        </ReactModal>  
+		    <button className="btn PlaceSave" onClick={this.saveUp.bind(this)}>Next</button>
+      	</div>
+
     );
   }
 }
--- src/Stuff.js
+++ src/Stuff.js
@@ -112,37 +112,7 @@ class Stuff extends Component {
 				      	<button className="btn btn-success" type="button" onClick={this.addStuff.bind(this)}>Add</button>
 				      </div>
 	        </ReactModal>			
-				<div className="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-				  <div className="modal-dialog" role="document">
-				    <div className="modal-content">
-				      <div className="modal-header">
-				        <h4 className="modal-title" id="myModalLabel">Dodaj przedmiot</h4>
-				        <span className="close pull-right" data-dismiss="modal" aria-hidden="true">&times;</span>
-				      </div>
-				      <div className="modal-body">
-							<div className="modal-form-container">
-							<form>
-								<div className="form-group">
-									<label>Nazwa:</label>
-									<input type="text" className="form-control PlaceText" onChange={stuffName => this.setState({ stuffName:stuffName.target.value })} value={this.state.stuffName}/>
-								</div>
-								<div className="form-group">
-									<label>Cena:</label>
-									<input type="text" className="form-control PlaceText" onChange={stuffPrice => this.setState({ stuffPrice:stuffPrice.target.value })} value={this.state.stuffPrice}/>
-								</div>
-								<div className="form-group">
-									<label>Ilość:</label>
-									<input type="text" className="form-control PlaceText" onChange={stuffAmount => this.setState({ stuffAmount:stuffAmount.target.value })} value={this.state.stuffAmount}/>
-								</div>
-							</form> 
-							</div>
-				      </div>
-				      <div className="modal-footer">
-				      	<button className="btn btn-success" type="button" onClick={this.addStuff.bind(this)}>Add</button>
-				      </div>
-				    </div>
-				  </div>
-				</div>
+				
 				<button className="btn pull-right PlaceSave" onClick={this.saveUp.bind(this)}>Next</button>
       		</div>
     );
