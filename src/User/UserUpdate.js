1ee7ff8937b91637a641b4e345f10319a192d45e
yerevin <sirmmate@gmail.com>
2017-02-18 17:24:33 +0100

    change password implementation

--- src/User/UserUpdate.js
+++ src/User/UserUpdate.js
@@ -38,7 +38,7 @@ class User extends Component {
         method: 'PUT',
         body: JSON.stringify({
             name: this.state.name,
-			age: this.state.age
+			      age: this.state.age
             
         })
       })
@@ -53,6 +53,33 @@ class User extends Component {
         this.setState({updateActivity: false});
       });
   
+    }
+
+  changePassword() {
+    var userId = localStorage.getItem("userId");
+      fetch(`http://localhost:8000/user/${userId}/change-password`,{
+          headers: {
+            'Accept': 'application/json',
+             'Content-Type': 'application/json'
+          },
+          method: 'PUT',
+          body: JSON.stringify({
+            oldpassword: this.state.oldPassword,
+            password: this.state.newPassword,
+            password_confirmation: this.state.newPasswordConfirmation,
+              
+          })
+        })
+        .then(
+            response => {
+                  const status = response.status;
+                  if (status === 200) {
+                    return response.json();
+                  }
+        })
+        .then(responseData =>{
+          this.setState({updateActivity: false});
+        });
     }
 
   componentWillMount() {
@@ -73,7 +100,20 @@ class User extends Component {
 					<label>Age:</label>
 					<input className="form-control" onChange={age => this.setState({ age:age.target.value, updateActivity: true })} value={this.state.age} />
 				</div>
+        <div className="form-group">
+          <label>Old password:</label>
+          <input className="form-control" onChange={oldPassword => this.setState({ oldPassword:oldPassword.target.value, updateActivity: true })} value={this.state.oldPassword} />
+        </div>
+        <div className="form-group">
+          <label>New password:</label>
+          <input className="form-control" onChange={newPassword => this.setState({ newPassword:newPassword.target.value, updateActivity: true })} value={this.state.newPassword} />
+        </div>
+        <div className="form-group">
+          <label>Repeat new password:</label>
+          <input className="form-control" onChange={newPasswordConfirmation => this.setState({ newPasswordConfirmation:newPasswordConfirmation.target.value, updateActivity: true })} value={this.state.newPasswordConfirmation} />
+        </div>
 			</form>
+      {(this.state.updateActivity) ? <button className="btn pull-right" onClick={this.changePassword.bind(this)}>Change password</button> : null}
 			{(this.state.updateActivity) ? <button className="btn pull-right" onClick={this.updateUser.bind(this)}>Save</button> : null}
 	      </div>
 	    </div>
